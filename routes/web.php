aaf12f5d70c400e888586eedbfb264cafe3cbe65
EdsonMorroNogueira <edson.nogueira@sptech.school>
2024-10-13 15:33:29 -0300

    feat(): Adds Livewire Components

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ProjectsController;
 
-Route::get('/', WelcomeController::class);
+Route::get('/', [ProjectsController::class, 'index'])->name('projects.index');
+
+Route::get('/projects/{project}', [ProjectsController::class, 'show'])->name('projects.show');
